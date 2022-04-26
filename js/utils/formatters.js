ca2ba9deeffd064048057344d8cecb4d04ebb836
coder_of_love <j.m.n.90@bk.ru>
2022-04-26 13:24:49 +0300

    :bricks: Add Util JS Formatter "conjugateQuantityRus"

---
+++ js/utils/formatters.js
@@ -0,0 +1,19 @@
+/**
+ * Conjugates a word in russian, depending on the quantity of the subject
+ * - example: conjugateQuantityRus(5, [котенок, котенка, котят]) === 5 котят
+ *
+ * @param {!Number} quantity
+ * @param {!String[]} titles - ordered array of word's numeric conjugations [titleForOne, titleForTwo, titleForFive]
+ * @returns {*}
+ */
+export function conjugateQuantityRus(quantity, titles) {
+	const cases = [2, 0, 1, 1, 1, 2];
+	if (!titles)
+		return;
+
+	return titles[
+		quantity % 100 > 4 && quantity % 100 < 20
+			? 2
+			: cases[quantity % 10 < 5 ? quantity % 10 : 5]
+	];
+}

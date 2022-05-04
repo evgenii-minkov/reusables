0e5d91fd7f70059fd0fb3e9e0b065bcc8d98e9b4
coder_of_love <j.m.n.90@bk.ru>
2022-05-04 12:47:19 +0300

    :sparkles: Update "mapDateToDurationVerbose" - Add ability to pass own formatter

--- js/lib/dates.js
+++ js/lib/dates.js
@@ -2,30 +2,32 @@
  * date-fns
  **/
 
- import intervalToDuration from "date-fns/intervalToDuration";
- import formatDuration from "date-fns/formatDuration";
- import enUS from "date-fns/locale/en-US";
- 
- import { validateDate } from "@utils/validators"; // TODO implement this
- 
- 
- const DEFAULT_LOCALE = enUS;
- const DEFAULT_DURATION_FORMAT = ["years", "months", "days"];
- 
- export const mapDateToDuration = (date) => {
-	 if(!validateDate(date))
-		 return null;
- 
-	 return intervalToDuration({
-		 start: new Date(date),
-		 end: new Date(),
-	 });
- };
- 
- export const mapDateToDurationVerbose = (date, { format = DEFAULT_DURATION_FORMAT,locale = DEFAULT_LOCALE } = {}) => {
-	 const duration = mapDateToDuration(date);
-	 if(!duration)
-		 return "";
- 
-	 return formatDuration(duration, { format, locale });
- };
+import intervalToDuration from "date-fns/intervalToDuration";
+import formatDuration from "date-fns/formatDuration";
+import enUS from "date-fns/locale/en-US";
+
+import { validateDate } from "@utils/validators"; // TODO implement this
+
+
+const DEFAULT_LOCALE = enUS;
+const DEFAULT_DURATION_FORMAT = ["years", "months", "days"];
+
+export const mapDateToDuration = (date) => {
+	if (!validateDate(date))
+		return null;
+
+	return intervalToDuration({
+		start: new Date(date),
+		end: new Date(),
+	});
+};
+
+export const mapDateToDurationVerbose = (date, { formatter, format = DEFAULT_DURATION_FORMAT, locale = DEFAULT_LOCALE } = {}) => {
+	const duration = mapDateToDuration(date);
+	if (!duration)
+		return "";
+
+	formatter = typeof formatter === "function" ? formatter : formatDuration;
+
+	return formatter(duration, { format, locale });
+};

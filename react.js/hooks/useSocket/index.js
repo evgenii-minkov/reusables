d741a0cbc185921ed6a5e56dbc788a7de1cb2ca4
coder_of_love <j.m.n.90@bk.ru>
2022-05-10 15:48:05 +0300

    :recycle: React.js Hook "useSocket"

--- react.js/hooks/useSocket/index.js
+++ react.js/hooks/useSocket/index.js
@@ -6,31 +6,54 @@ import SocketIO from "socket.io-client";
 const API_URL_SOCKETS = "/" + process.env.API_URL_SOCKETS;	// TODO define this
 const HOST = process.env.HOST;								// TODO and this
 
+// next.js version
+const socket = SocketIO.connect(HOST, { path: API_URL_SOCKETS });
+// express.js version
+// const socket = SocketIO.io("/");
+
 
 export default function useSocket(props) {
 	// connect to socket.io-server when props.handlers change
 	useEffect(() => {
-		if(!props.handlers || typeof props.handlers !== "object" || Object.keys(props.handlers).length === 0)
+		// sanity check
+		if (!socket)
 			return;
 
-		// connecting
-		const socketReceiver = SocketIO.connect(HOST, { path: API_URL_SOCKETS });
+		if (!props.handlers || typeof props.handlers !== "object" || Object.keys(props.handlers).length === 0)
+			return;
 
 		// defining handlers for each message passed as a key in props.handlers
-		for(const message of Object.keys(props.handlers)) {
-			if(typeof props.handlers[message] !== "function")
+		for (const message of Object.keys(props.handlers)) {
+			if (typeof props.handlers[message] !== "function")
 				continue;
 
-			socketReceiver.on(message, props.handlers[message]);
+			socket.on(message, props.handlers[message]);
 		}
 
-		// if needed, disconnecting from socket.io-server on unmount
-		return () => props.toDisconnectOnUnmount && socketReceiver ? socketReceiver.disconnect() : true;
+		return () => {
+			// sanity check
+			if (!socket)
+				return;
+
+			// removing previously added listeners
+			for (const message of Object.keys(props.handlers)) {
+				if (typeof props.handlers[message] !== "function")
+					continue;
+
+				socket.off(message, props.handlers[message]);
+			}
+
+			// if needed, disconnecting from socket.io-server on unmount
+			if(props.toDisconnectOnUnmount)
+				socketReceiver.disconnect();
+		};
 	}, [props.handlers]);
+
+	return socket;
 }
 
 useSocket.defaultProps = {
-	toDisconnectOnUnmount: true,
+	toDisconnectOnUnmount: false,
 };
 useSocket.propTypes = {
 	toDisconnectOnUnmount: PropTypes.bool,
